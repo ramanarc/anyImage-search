cd152ec718e9cd4a2196687922314469be0b1eb5
Anuj Raman <anujramanc@gmail.com>
2023-05-31 11:59:30 +0530

    fetch request

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const formTag = document.querySelector('form')
 const inputTag = formTag.querySelector('input')
 
 
+
+const url = 'https://api.unsplash.com/search/photos?per_page=24&query=blue'
+
 // Search Unsplash API
 function searchUnsplash(term) {
-  console.log(term);
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data)
+    })
 }
 
 formTag.addEventListener('submit', function (event) {
