d4e0702d3843563cfddc08029f0568a4f039f3cd
Anuj Raman <anujramanc@gmail.com>
2023-05-31 14:26:26 +0530

    final commit

--- main.js
+++ main.js
@@ -1,22 +1,62 @@
 const formTag = document.querySelector('form')
 const inputTag = formTag.querySelector('input')
+const resultsTag = document.querySelector('.results');
 
 
 
-const url = 'https://api.unsplash.com/search/photos?per_page=24&query=blue'
+const url = 'https://api.unsplash.com/search/photos?per_page=24&query='
 
 // Search Unsplash API
 function searchUnsplash(term) {
-  return fetch(url)
+  return fetch(url + term, {
+    method: "GET",
+    headers: {
+      "Authorization": "Client-ID " + accessKey
+    },
+  })
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
+      // Format unsplash results to suit our needs
+      return data.results.map(result => {
+        return {
+          imgSrc: result.urls.regular,
+          userName: result.user.name,
+          // likes: result.likes,
+          altDescription: result.alt_description,
+          backgroundColor: (result.color || '#eee') + "cc"
+
+        }
+      })
     })
 }
 
+
+// Add fetched results to the page
+
+const addResults = function (results) {
+  // remove all loading tags
+  resultsTag.innerHTML = "";
+
+  results.forEach(result => {
+    resultsTag.innerHTML = resultsTag.innerHTML + `
+    <div class="result-box">
+      <div class="image" style="background-color: ${result.backgroundColor}"><img src="${result.imgSrc}" alt=""></div>
+      <div class="content-container">
+        <h4 style="color: #171717">${result.altDescription}</h4>
+        <p style="color: #404040">by ${result.userName}</p>
+      </div>
+    </div>
+    `
+  })
+}
+
+
 formTag.addEventListener('submit', function (event) {
   const searchTerm = inputTag.value
-  searchUnsplash(searchTerm);
+  searchUnsplash(searchTerm)
+    .then(results => {
+      addResults(results)
+    })
 
   event.preventDefault();
 })
